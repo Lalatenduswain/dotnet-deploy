a4e5cf194a0e142b6234525f3cd61069c8b5760a
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-07-14 20:42:46 +0530

    Update install-dotnet.sh

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt upgrade
 
 # Install dependencies
-sudo apt install curl libunwind8 gettext
+sudo apt install curl libunwind8 gettext libc6 libgcc1 libgssapi-krb5-2 libicu66 libssl1.1 libstdc++6 zlib1g
 
 # Download the .NET SDK
 wget https://download.visualstudio.microsoft.com/download/pr/62181759-93ce-4512-8de1-92de74a6ba3f/f83ea41c3161f301d3584598f9c31801/dotnet-sdk-6.0.412-linux-x64.tar.gz
