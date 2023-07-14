f59aa1e9b9f7785c01ea5d03dbcb527ff378480f
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-07-14 20:38:48 +0530

    Create install-dotnet.sh

---
+++ install-dotnet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update the system
+sudo apt update
+sudo apt upgrade
+
+# Install dependencies
+sudo apt install curl libunwind8 gettext
+
+# Download the .NET SDK
+wget https://download.visualstudio.microsoft.com/download/pr/62181759-93ce-4512-8de1-92de74a6ba3f/f83ea41c3161f301d3584598f9c31801/dotnet-sdk-6.0.412-linux-x64.tar.gz
+
+# Extract and install the .NET SDK
+mkdir dotnet
+tar -xf dotnet-sdk-6.0.412-linux-x64.tar.gz -C dotnet
+sudo mv dotnet /usr/local
+
+# Set the PATH environment variable
+echo 'export PATH="$PATH:/usr/local/dotnet"' >> ~/.bashrc
+source ~/.bashrc
+
+# Verify the installation
+dotnet --version
